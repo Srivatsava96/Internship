fcfbd4381aff8aab3c3d789133c66e869ebde5d4
Srivatsava <srivatsavagummalla@gmail.com>
2017-05-08 22:23:58 +0530

    Test file

---
+++ Source/fruit_bot.py
@@ -0,0 +1,16 @@
+import bottle
+
+@bottle.route('/')
+def home_page():
+	mythings=['apple','orange','banana','peach']
+	return bottle.template('hello_world',{'username' : "Richard", 'things' : mythings})
+
+@bottle.post('/favourite_fruit')
+def favourite_fruit():
+	fruit = bottle.request.forms.get("fruit")
+	if(fruit == None or fruit == ""):
+		fruit = "No fruit selected"
+	return bottle.template('fruit_selection.tpl',{'fruit':fruit})
+
+bottle.debug(True)
+bottle.run(host='localhost',port=8082)
