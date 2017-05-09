058f584e54709f2033d3702efc9266af9fec5fb5
Srivatsava <srivatsavagummalla@gmail.com>
2017-05-09 19:58:05 +0530

    Data model for patient

--- Source/patient.py
+++ Source/patient.py
@@ -1,7 +1,22 @@
 # -----------------------------------------------------------------------------
 # Description: Data model for patient
 # Author: Srivatsava
-# Date: 08-08-2017
+# Date: 08-05-2017
 #------------------------------------------------------------------------------
 
-
+class Patient:
+	patCount = 0
+	
+	def __init__(self,number,gender,title,given_name,midinitial,surname,street_addr,city,state,zip,birthday):
+		self.number = number
+		self.gender = gender
+		self.title = title
+		self.given_name = given_name
+		self.midinitial = midinitial
+		self.surname = surname
+		self.street_addr = street_addr
+		self.city = city
+		self.state = state
+		self.zip = zip
+		self.birthday = birthday
+		Patient.patCount += 1
