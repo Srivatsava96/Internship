eeb9697f371ee17fd6a07c4aeecd4a223936e7de
Srivatsava <srivatsavagummalla@gmail.com>
2017-05-10 23:22:32 +0530

    Reading Patient data from .csv file using python

--- Source/patientDataParser.py
+++ Source/patientDataParser.py
@@ -5,3 +5,12 @@
 #------------------------------------------------------------------------------
 
 
+import csv
+import pymongo
+
+#connection = pymongo.MongoClient('localhost',27017)
+f = open('FakeNameGenerator.com_3c6159aa.csv', encoding='utf_8')
+reader = csv.reader(f)
+for row in reader:
+    print (row)
+f.close()
