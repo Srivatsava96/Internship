fb0c8d0a7d7cc746e263e86b0ceedef5a948c446
Srivatsava <srivatsavagummalla@gmail.com>
2017-07-04 17:10:34 +0530

    #17 Command line parameters inserted for data import. -i for file path, -c for clean import, -h for help and -r for drop database

--- Test/DataImport/PhysicianDataImport.py
+++ Test/DataImport/PhysicianDataImport.py
@@ -8,6 +8,8 @@
 import os
 import sys
 import csv
+from getopt import getopt
+#from Test.DataImport.icd10DataImport import dataPath
 
 proj_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 proj_path = os.path.dirname(proj_path)
@@ -17,50 +19,86 @@ if proj_path not in sys.path:
 from Source.cntCdApp import CdApp
 from Source.modelCdPhysician import Physician
 
-#1. Connect to Database
-CdApp.connectDb()
-
-#2. Get patient data file
-dataDir = CdApp.getAppConfig().get('DataDir')
-if not dataDir:
-	raise ImportError('Config file error, DataDir not defined')
-
-dataFile = CdApp.getAppConfig().get('PhyDataFile')
-if not dataFile:
-	raise ImportError('Config file error, DataFile not defined')
-
-dataPath = os.path.join(dataDir, dataFile)
 logger = CdApp.getLogger()
 
-#3. parse and save in database
-count = 0
-clean_import = True
-with open(dataPath, encoding='utf_8') as infile:
-	reader = csv.reader(infile)
-	for count,Phydict in enumerate(reader):
-		dbPhy = None
-		
-		if not count%2000:
-			print('{}'.format(count))
-		
-		if len(Phydict)!=329:
-			print('{}'.format(len(Phydict)))
-				
-		if not clean_import:
-			dbPhy = Physician.objects(NPI=Phydict[0]).first()
+def get_dataPath():
+	
+	#2. Get npi data file
+	dataDir = CdApp.getAppConfig().get('DataDir')
+	if not dataDir:
+		raise ImportError('Config file error, DataDir not defined')
+	
+	dataFile = CdApp.getAppConfig().get('PhyDataFile')
+	if not dataFile:
+		raise ImportError('Config file error, DataFile not defined')
+	
+	dataPath = os.path.join(dataDir, dataFile)
+	return dataPath
+#------------------------------------------------------------------------------
+
+def import_data(data_path, clean_import, delete_col):
+	if (delete_col == True):
+		Physician.drop_collection()
+		return
+	with open(data_path, encoding='utf_8') as infile:
+		reader = csv.reader(infile)
+		for count,Phydict in enumerate(reader):
+			dbPhy = None
 			
-		if dbPhy:
-			# 2. if exist use the same physician to update
-			dbPhy.UpdateUsingDict(Phydict = Phydict)
-			logger.info('is the time of update of the phy %s to database',
-                         Phydict[0])
-			dbPhy.save()
-		else:	
-			# 3. if not, create new physician and update
-			ndbPhy = Physician()
-			ndbPhy.UpdateUsingDict(Phydict = Phydict)
-			logger.info('is the time of insert of the phy %s to database',
-                         Phydict[0])
-			ndbPhy.save()
+			if not count%2000:
+				print('{}'.format(count))
+			
+			if len(Phydict)!=329:
+				print('{}'.format(len(Phydict)))
+					
+			if not clean_import:
+				dbPhy = Physician.objects(NPI=Phydict[0]).first()
+				
+			if dbPhy:
+				# 2. if exist use the same physician to update
+				dbPhy.UpdateUsingDict(Phydict = Phydict)
+				logger.info('is the time of update of the phy %s to database',
+	                         Phydict[0])
+				dbPhy.save()
+			else:	
+				# 3. if not, create new physician and update
+				ndbPhy = Physician()
+				ndbPhy.UpdateUsingDict(Phydict = Phydict)
+				logger.info('is the time of insert of the phy %s to database',
+	                         Phydict[0])
+				ndbPhy.save()
+#------------------------------------------------------------------------------
+			
+def main(argv):
+	try:
+		opts, args = getopt(argv,"hi:cr",["help="])
+		
+	except getopt.GetoptError:
+		print("PhysicianDataImport.py -i <inputfile> -c <cleanimport>")
+		print("-h <help> -r <dropdatabase>")
+		sys.exit(2)
+	
+	data_path = None
+	clean_import = False
+	delete_col = False
+	CdApp.connectDb()
+	data_path = get_dataPath()
+	for opt, arg in opts:
+		if opt == '-h':
+			print("-c <cleanimport>, -r <dropdatabase>, -i <filepath>")
+			sys.exit()
+		elif opt in ("-i", "--filepath"):
+			data_path = arg
+		elif opt in ("-c", "--cleanimport"):
+			clean_import = True
+		elif opt in ("-r", "--dropdatabase"):
+			delete_col = True
+
+
+	import_data(data_path, clean_import, delete_col)
+	print('Import done')
+
+if __name__ == "__main__":
+	main(sys.argv[1:])					
 #------------------------------------------------------------------------------
 
